641c285f9a2b233d3534505f8d5fdbeea82c9bcc
tawebster <tawebster42@tntech.edu>
2024-04-24 18:47:44 +0000

    Updated serverSetup.sh to include part of the outputs that will be appended to TicketID.log

--- serverSetup.sh
+++ serverSetup.sh
@@ -14,17 +14,30 @@ arrResults=$(curl ${strBaseURL} | jq)
 
 intCurrent=0
 intLength=$(echo ${arrResults} | jq 'length')
-echo ${intLength}
+#echo ${intLength}
 
 while [ $intCurrent -lt $intLength ];
 do
+hostname=$(hostname)
 strRequestor=$(echo ${arrResults} | jq -r .[${intCurrent}].requestor)
 strSubmissionDate=$(echo ${arrResults} | jq -r .[${intCurrent}].submissionDate)
+strStandardConfig=$(echo ${arrResults} | jq -r .[${intCurrent}].standardConfig)
 strSoftwarePack=$(echo ${arrResults} | jq -r .[${intCurrent}].softwarePackages)
 strAdditionalConfig=$(echo ${arrResults} | jq -r .[${intCurrent}].additionalConfigs)
-echo ${strRequestor}
-echo ${strSubmissionDate}
-echo ${strSoftwarePack}
-echo ${strAdditionalConfig}
+#echo ${strRequestor}
+#echo ${strSubmissionDate}
+#echo ${strStandardConfig}
+#echo ${strSoftwarePack}
+#echo ${strAdditionalConfig}
+
+echo "TicketID: ${strTicketID}"
+echo "Start DateTime: ${strSubmissionDate}"
+echo "Requestor: ${strRequestor}"
+echo "External IP Address: ${strIP}"
+echo "Hostname: ${hostname}"
+echo "Standard Configuration: ${strStandardConfig}"
+echo ""
+echo "softwarePackage - ${strSoftwarePack}"
+echo "addtionalConfig - ${strAddtionalConfig}"
 ((intCurrent++))
 done
